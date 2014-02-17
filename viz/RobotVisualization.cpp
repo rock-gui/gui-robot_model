a70d3aaacdbfdab354c233fd45e210dda5cc3eef
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2014-02-17 15:53:54 +0100

    RobotVisualization: joints size frames method as a Qt property is now available in the class in order to allow a desirable size of the RBS spheres and frame axis.

--- viz/RobotVisualization.cpp
+++ viz/RobotVisualization.cpp
@@ -19,6 +19,7 @@ struct RobotVisualization::Data {
 RobotVisualization::RobotVisualization()
     : p(new Data)
     , framesEnabled_(false)
+    , joints_size(0.1)
 {
 }
 
@@ -51,11 +52,11 @@ void RobotVisualization::setModelFile(QString modelFile)
                 new vizkit3d::RigidBodyStateVisualization(this);
         frame->setPluginName(QString::fromStdString(segments[i]));
         frame->setPluginEnabled(framesEnabled_);
-        frame->resetModel(0.2);
+        frame->resetModel(joints_size);
         segment->getGroup()->addChild(frame->getRootNode());
         _frameVisualizers[segments[i]] = frame;
     }
-    
+
     emit childrenChanged();
 }
 
@@ -96,6 +97,20 @@ void RobotVisualization::updateMainNode ( osg::Node* node )
 {
     //osg::Geode* geode = static_cast<osg::Geode*>(node);
     // Update the main node using the data in p->data
+}
+
+
+double RobotVisualization::getJointsSize() const
+{
+    return joints_size;
+}
+
+
+void RobotVisualization::setJointsSize(double size)
+{
+    joints_size = size;
+    for (map<string, RigidBodyStateVisualization*>::iterator it = _frameVisualizers.begin(); it != _frameVisualizers.end(); ++it)
+        it->second->resetModel(joints_size);
 }
 
 void RobotVisualization::updateDataIntern(base::samples::Joints const& value)
