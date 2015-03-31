a9b659c9d047cc2743b55cd117dc3d158441578b
Malte Wirkus <malte.wirkus@dfki.de>
2015-03-31 17:39:46 +0200

    updated test program to test text display and transform calculation

---
+++ test/my_timer.h
@@ -0,0 +1,28 @@
+#include <QTimer>
+#include "vizkit3d/RobotVisualization.hpp"
+#include <osg/io_utils>
+
+extern vizkit3d::RobotVisualization* plugin;
+
+class MyTimer :public QTimer{
+    Q_OBJECT
+public:
+    MyTimer(){}
+    ~MyTimer(){}
+
+public slots:
+    void update_transform(){
+        std::string source_frame = "Hand_r";
+        std::string target_frame = "Chest";
+
+        osg::Matrixd t=plugin->getRelativeTransform(source_frame, target_frame);
+        std::stringstream ss;
+        ss << source_frame << " -> " << target_frame << " :\nT: " << t.getTrans()<<"\n";
+        ss << "R: "<<t.getRotate()<<"\n";
+
+        std::cout << ss.str() << std::endl;
+        plugin->getSegment(source_frame)->attachTextLabel(ss.str());
+        plugin->setFrameEnabled(source_frame.c_str(), true, 0.05);
+        plugin->setFrameEnabled(target_frame.c_str(), true, 0.05);
+    }
+};
--- test/plugintest.cpp
+++ test/plugintest.cpp
@@ -6,6 +6,12 @@
 #include <vizkit3d/QtThreadedWidget.hpp>
 #include <vizkit3d/Vizkit3DWidget.hpp>
 #include <vizkit3d/Vizkit3DPlugin.hpp>
+#include "my_timer.h"
+
+vizkit3d::RobotVisualization* plugin;
+
+
+
 
 int main(int argc, char** argv)
 {
@@ -13,7 +19,7 @@ int main(int argc, char** argv)
     vizkit3d::Vizkit3DWidget *widget = new vizkit3d::Vizkit3DWidget();
     widget->show();
 
-    vizkit3d::RobotVisualization* plugin = new vizkit3d::RobotVisualization();
+    plugin = new vizkit3d::RobotVisualization();
     plugin->setModelFile("/media/data/Development/besman/bundles/besman_test/data/aila.urdf");
     widget->addPlugin(plugin);
 
@@ -21,9 +27,12 @@ int main(int argc, char** argv)
     joints.resize(2);
     joints.names[0] = "J_Knees";
     joints.names[1] = "J_Hip";
-    joints.elements[0].position = 1.4;
-    joints.elements[1].position = 1.4;
+    joints.elements[0].position = 2.1;
+    joints.elements[1].position = 2.1;
     plugin->updateData(joints);
+
+    MyTimer t;
+    t.singleShot(500, &t, SLOT(update_transform()));
 
     app.exec();
 
