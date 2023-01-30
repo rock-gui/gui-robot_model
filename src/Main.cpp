27234bf5988a5b1cd3309507aa71f1d468b08206
Anna Born <anna.born@dfki.de>
2023-01-30 12:41:53 +0100

    add control ui

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,10 @@
 #include "vizkit3d/Vizkit3DWidget.hpp"
 #include <vizkit3d/RobotVisualization.hpp>
+#include <control_ui/ControlUi.h>
 #include <QApplication>
+#include <QDockWidget>
+#include <QObject>
+#include <base/samples/Joints.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -9,27 +13,39 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    std::cout << "loading file: " << argv[1] << std::endl;
-    std::string filepath(argv[1]);
+    QString filepath(argv[1]);
+    std::cout << "Loading urdf file: " << filepath.toStdString() << std::endl;
+
 
     QApplication a(argc, argv);
-    vizkit3d::Vizkit3DWidget window;
 
-    // just for debug purposes
-    QStringList* plugins = window.getAvailablePlugins();
-    for ( const auto& i : *plugins  )
-    {
-        std::cout << i.toStdString() << std::endl;
-    }
+    // ---- load Vizkit3D and RobotVisualization plugin
+    vizkit3d::Vizkit3DWidget *vizkitWidget = new vizkit3d::Vizkit3DWidget();
 
-    // load RobotVisualization plugin
-    QObject* robot_viz = window.loadPlugin("robot_model","");
-    vizkit3d::RobotVisualization* vizPlugin = dynamic_cast<vizkit3d::RobotVisualization *>(robot_viz);
+    QObject* robotViz = vizkitWidget->loadPlugin("robot_model","");
+    if(robotViz == nullptr)
+        throw std::runtime_error("Could not load 'robot_model' plugin");
 
-    // load the file
-    vizPlugin->loadFromFile(QString(filepath.c_str()));
+    vizkit3d::RobotVisualization* vizPlugin = dynamic_cast<vizkit3d::RobotVisualization *>(robotViz);
+    if(vizPlugin == nullptr)
+        throw std::runtime_error("Could not cast the plugin into RobotVisualization.");
 
-    window.show();
+    // load the model file
+    vizPlugin->setModelFile(filepath);
+
+
+    // ----- load control ui
+    ControlUi *controlWidget = new ControlUi();
+    controlWidget->initFromURDF(filepath);
+
+    QDockWidget *controlDockWidget = new QDockWidget();
+    controlDockWidget->setWidget(controlWidget);
+
+    vizkitWidget->addDockWidget(Qt::BottomDockWidgetArea, controlDockWidget);
+
+    QObject::connect(controlWidget, SIGNAL(newVal(base::samples::Joints)),vizPlugin,SLOT(setJointsState(base::samples::Joints)));
+
+    vizkitWidget->show();
 
     return a.exec();
 }
